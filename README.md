69dd67a133158579739c73570adedaf689fa58d5
thuatdt137 <dothuat098@gmail.com>
2025-01-15 17:10:46 +0700

    slot 3

---
+++ README.md
@@ -0,0 +1,15 @@
+
+# Project Title
+
+This repo references all examples, labs, and projects in my learning .net process.
+
+
+## Lessons Learned
+
+Chapter 1. Networking programming
+    Client App
+    ServerApp
+    slot1_DemoHttpClient
+    webrequest_res_demo
+Chapter 2. Asynchronous and Parallel Programming in .Net
+
---
+++ Slot2_UsingTaskDemonstration01/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Slot2_UsingTaskDemonstration01
+{
+    internal class Program
+    {
+        static void PrintNumber(string message)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{message}:{i}");
+                Thread.Sleep(1000);
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            Thread.CurrentThread.Name = "Main";
+            Task task01 = new Task(() => PrintNumber("Task 01"));
+            task01.Start();
+            Task task02 = Task.Run(delegate { PrintNumber("Task 02"); });
+            Task task03 = new Task(new Action(() =>
+            {
+                PrintNumber("Task 03");
+            }));
+            task03.Start();
+            Console.WriteLine($"Thread '{Thread.CurrentThread.Name}'");
+            Console.ReadKey();
+        }
+    }
+}
